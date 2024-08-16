a51f839cb95e87bb2b55bbcbcfa41c7bf74b3eca
Tu Nombre <tuemail@example.com>
2024-08-16 02:39:18 +0200

    Feat: Hero section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,46 @@
-import Link from "next/link";
+import Link from 'next/link'
 
-import { LatestPost } from "@/app/_components/post";
-import { api, HydrateClient } from "@/trpc/server";
+import { LatestPost } from '@/app/_components/post'
+import { api, HydrateClient } from '@/trpc/server'
+import { Badge } from '@/components/ui/badge'
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+    const hello = await api.post.hello({ text: 'from tRPC' })
 
-  void api.post.getLatest.prefetch();
+    void api.post.getLatest.prefetch()
 
-  return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-          </div>
-
-          <LatestPost />
-        </div>
-      </main>
-    </HydrateClient>
-  );
+    return (
+        <HydrateClient>
+            <main className="w-full py-20 lg:py-40">
+                <div className="container mx-auto">
+                    <div className="flex flex-col-reverse lg:flex-row gap-10 lg:items-center">
+                        <div className="bg-muted rounded-md w-full aspect-video h-full flex-1"></div>
+                        <div className="flex gap-4 pl-0 lg:pl-20 flex-col  flex-1">
+                            <div>
+                                <Badge>Platform</Badge>
+                            </div>
+                            <div className="flex gap-2 flex-col">
+                                <h2 className="text-xl  md:text-5xl tracking-tighter lg:max-w-xl font-regular text-left">
+                                    This is the start of something new
+                                </h2>
+                                <p className="text-lg max-w-xl lg:max-w-sm leading-relaxed tracking-tight text-muted-foreground text-left">
+                                    Managing a small business today is already
+                                    tough. Avoid further complications by
+                                    ditching outdated, tedious trade methods.
+                                    Our goal is to streamline SMB trade, making
+                                    it easier and faster than ever.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="flex flex-col items-center gap-2">
+                    <p className="text-2xl text-white">
+                        {hello ? hello.greeting : 'Loading tRPC query...'}
+                    </p>
+                </div>
+                <LatestPost /> */}
+            </main>
+        </HydrateClient>
+    )
 }
